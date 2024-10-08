2fa1c4471f80ac6e8a7304e136d7fc050d0c46af
exozy <exozy.nebulous@gmail.com>
2024-10-08 20:05:42 +0500

    added search orders

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 const table = document.querySelector("table");
+
+const foundOrder = document.getElementById("searchInput");
+const clickFindOrder = document.getElementById("searchbtn");
+
+clickFindOrder.addEventListener("click", async () => {
+  const searchValue = foundOrder.value.toLowerCase();
+  if (searchValue) {
+    await searchOrder(searchValue);
+  }
+});
+
+async function searchOrder(orderId) {
+  try {
+    const response = await fetch(`http://localhost:3000/order/${orderId}`);
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+    const data = await response.json();
+    table.innerHTML = `
+              <tr>
+                <td>${data.number}</td>
+                <td>${data.startDate}</td>
+                <td>${data.endDate}</td>
+                <td>${data.device}</td>
+                <td>${data.issue}</td>
+                <td>${data.description}</td>
+                <td>${data.client}</td>
+                <td>${data.status}</td>
+                <td>${data.master}</td>
+                <td>${data.comments}</td>
+              </tr>
+            `;
+  } catch (error) {
+    console.error(error);
+  }
+}
 async function orders() {
   try {
     const response = await fetch("http://localhost:3000/orders");
